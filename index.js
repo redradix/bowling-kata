ffc799f6ad363032be06cfc5f836fa4bcdddd056
Miguel <hugo.hernandez.estevez.contractor@bbva.com>
2019-06-14 17:02:40 +0200

    311-5--------------- green

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const rollToNumber = roll => Number(roll) || 0
 
 const calculateScore = input => {
   const score =
-    rollToNumber(input[0]) + rollToNumber(input[1]) + rollToNumber(input[2])
+    rollToNumber(input[0]) +
+    rollToNumber(input[1]) +
+    rollToNumber(input[2]) +
+    rollToNumber(input[4])
   return score
 }
 module.exports = calculateScore
