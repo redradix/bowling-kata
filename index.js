7cbd1fe2349f25e3bea2de2f4d96a2c11a0ce35c
Miguel <hugo.hernandez.estevez.contractor@bbva.com>
2019-06-14 17:29:54 +0200

    1/------------------ refactor

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const rollToNumber = roll => Number(roll) || 0
+const SPARE = '/'
+const MAX_SCORE_PER_ROLL = 10
+
+const isSpare = roll => roll === SPARE
 
 const calculateScore = input => {
   const rolls = input.split('')
-  return rolls.reduce((sum, roll, index) => {
+  return rolls.reduce((score, roll, index) => {
     const lastRollScore = rollToNumber(rolls[index - 1])
-    if (roll === '/') roll = 10 - lastRollScore
-    return sum + rollToNumber(roll)
+    if (isSpare(roll)) {
+      return score + MAX_SCORE_PER_ROLL - lastRollScore
+    }
+    return score + rollToNumber(roll)
   }, 0)
 }
 module.exports = calculateScore
