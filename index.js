baeaa77aa0dac118dad8c3095037ac32b3216dba
Miguel <hugo.hernandez.estevez.contractor@bbva.com>
2019-06-14 16:37:20 +0200

    -1------------------ green

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const calculateScore = input => {
   if (input.startsWith('1')) return 1
   if (input.startsWith('2')) return 2
+  if (input[1] === '1') return 1
   return 0
 }
 module.exports = calculateScore
