b1108eaea4209aca1c5a5a31d76bc0cb8f0759fb
Miguel <hugo.hernandez.estevez.contractor@bbva.com>
2019-06-14 17:05:06 +0200

    311-5--------------- refactor

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 const rollToNumber = roll => Number(roll) || 0
 
 const calculateScore = input => {
-  const score =
-    rollToNumber(input[0]) +
-    rollToNumber(input[1]) +
-    rollToNumber(input[2]) +
-    rollToNumber(input[4])
-  return score
+  const rolls = input.split('')
+  return rolls.reduce((sum, roll) => sum + rollToNumber(roll), 0)
 }
 module.exports = calculateScore
