cb58aa8104445e2a2c9041538beadcab3288f7e4
Miguel <hugo.hernandez.estevez.contractor@bbva.com>
2019-06-14 17:49:57 +0200

    X------------------ refactor

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const rollToNumber = roll => Number(roll) || 0
 const SPARE = '/'
+const STRIKE = 'X'
 const MAX_SCORE_PER_ROLL = 10
 
 const isSpare = roll => roll === SPARE
+const isStrike = roll => roll === STRIKE
 
 const calculateScore = input => {
   const rolls = input.split('')
   return rolls.reduce((score, roll, index) => {
     const prevRollScore = rollToNumber(rolls[index - 1])
     const nextRollScore = rollToNumber(rolls[index + 1])
-    if (roll === 'X') {
+    if (isStrike(roll)) {
       return score + MAX_SCORE_PER_ROLL
     }
     if (isSpare(roll)) {
