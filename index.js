89840ac9f6cbf8918a2cb3dfea4d864d780a0f78
Miguel <hugo.hernandez.estevez.contractor@bbva.com>
2019-06-14 17:21:36 +0200

    1/------------------ green

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const rollToNumber = roll => Number(roll) || 0
 
 const calculateScore = input => {
   const rolls = input.split('')
-  return rolls.reduce((sum, roll) => {
-    if (roll === '/') roll = 10
+  return rolls.reduce((sum, roll, index) => {
+    const lastRollScore = rollToNumber(rolls[index - 1])
+    if (roll === '/') roll = 10 - lastRollScore
     return sum + rollToNumber(roll)
   }, 0)
 }
